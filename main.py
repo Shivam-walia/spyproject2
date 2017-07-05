5393e089909aea21bb5976246b1122ac2c4b3568
shivam-walia <shivamw65@gmail.com>
2017-07-05 21:56:01 +0530

    main.py file is added

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 #main.py file is created
 
-from self_info import  self_info
+#from self_info import  self_info
 #function calling
-self_info()
+#self_info()
+#from  get_user_info import get_user_info
+#insta_username=raw_input("Enter the user name of user:")
+#get_user_info(insta_username)
+
+#from get_user_id import get_user_id
+#insta_username = raw_input("Enter the username of the user: ")
+#get_user_id(insta_username)
+#function call for own post
+#from get_my_post import get_my_post
+#get_my_post()
+
+
+#function calling for user post
+
+from get_user_post import get_user_post
+insta_username=raw_input("Please enter user name")
+get_user_post(insta_username)
