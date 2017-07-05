490cfadd8022bb3e54403fb009f4c208947e1870
shivam-walia <shivamw65@gmail.com>
2017-07-05 12:45:55 +0530

    self_info.py user info added

--- self_info.py
+++ self_info.py
@@ -5,3 +5,15 @@ def self_info():
     request_url=(BASE_URL+ 'users/self/?access_token=%s') %(APP_ACCESS_TOKEN)
     print 'Get request url= %s' %(request_url)
     user_info=requests.get(request_url).json()
+
+    if user_info['meta']['code']==200:
+        #conitional check
+        if len(user_info['data']):
+            print 'username : %s' %(user_info['data']['username'])
+            print 'No. of followers: %s' %(user_info['data']['counts']['followed_by'])
+            print 'No. of people you ae following %s' %(user_info['data']['counts']['follows'])
+            print 'No. of posts %s' %(user_info['data']['counts']['media'])
+        else:
+            print 'user do not exist'
+    else:
+        print'status code other tha 200 recieved'
