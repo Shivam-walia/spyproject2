1a6cec69b9d8e401994d054b91a8c5ec6e7cb8f3
shivam-walia <shivamw65@gmail.com>
2017-07-06 17:56:49 +0530

    post_comments.py file is added

--- post_comments.py
+++ post_comments.py
@@ -1,16 +1,17 @@
 #post comments initialised
 import requests
 from constants import  *
-from get_user_post import get_user_post
+from get_user_id import get_user_id
 def post_comment(insta_username):
 
-    media_id=get_user_post(insta_username)
+    media_id=get_user_id(insta_username)
     comment_txt=raw_input("Enter your comment here...:-")
 
-    request_url=(BASE_URL + 'media/%s/comments') + (media_id)
+    request_url=(BASE_URL + 'media/%s/comments') %(media_id)
     payload={"access_token:":APP_ACCESS_TOKEN,"text" : comment_txt}
     print 'POST request url : %s ' %(request_url)
     comment_on_post=requests.post(request_url,payload).json()
+    print comment_on_post
 
     if comment_on_post['meta']['code']==200:
         print'successfully add a new comment'
