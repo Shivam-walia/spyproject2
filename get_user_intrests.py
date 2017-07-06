6d4cb719cb744ca87a95c0e760637220fd910c73
shivam-walia <shivamw65@gmail.com>
2017-07-06 22:25:06 +0530

    get_user_intrests.py  is added fetch tags baddi university

--- get_user_intrests.py
+++ get_user_intrests.py
@@ -1,4 +1,15 @@
                                     #objective to take user intrests and plot using matplot library
+import  matplotlib
+#import matplotlib.pyplot as plt
+#labels ='High', 'low' 'none'
+#sizes=[60,30,10]
+#explode=(0.1,0,0)
+#fig1, ax1 = plt.subplots()
+#ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+ #       shadow=True, startangle=90)
+#ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+#plt.show()
 
 #Get user intrests initialised
 #Requests library added
@@ -11,21 +22,22 @@ from get_user_post import get_user_post
 def user_intrests(insta_username):
  # fetch current user post
     media_id=get_user_post(insta_username)
-    request_url=(BASE_URL + 'tags/%s/tag-name') %(APP_ACCESS_TOKEN)
+    request_url=(BASE_URL +'tags/search/?q=baddi_university&access_token=%s') %(APP_ACCESS_TOKEN)
     print "Get request url : %s" %(request_url)
     intrests_info=requests.get(request_url).json()
     print intrests_info
 
-   # if intrests_info['meta']['data']==200:
-    #    if len(intrests_info['data']):
+    if intrests_info['meta']['code']==200:
 
-            #return intrests_info['data'][0]['tags']
-        #else:
-            #return None
-    #else:
+        if len(intrests_info['data']):
 
-        #print 'Status code other than 200 received!'
-        #exit()
+            return intrests_info['data'][0]
+        else:
+            return None
+    else:
+
+        print 'Status code other than 200 received!'
+        exit()
 
 insta_username=raw_input("Enter the user name")
 
