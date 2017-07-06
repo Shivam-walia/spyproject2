89ebf945fd6260a58673021923219ffcc9e00bcb
shivam-walia <shivamw65@gmail.com>
2017-07-06 18:17:10 +0530

    post_comments.py added

--- post_comments.py
+++ post_comments.py
@@ -1,19 +1,17 @@
 #post comments initialised
 import requests
 from constants import  *
-from get_user_id import get_user_id
+from get_post_id import get_post_id
 def post_comment(insta_username):
 
-    media_id=get_user_id(insta_username)
-    comment_txt=raw_input("Enter your comment here...:-")
-
+    media_id=get_post_id(insta_username)
+    comment_text=raw_input("Enter your comment here...:-")
+    payload = {"access_token": APP_ACCESS_TOKEN, "text": comment_text}
     request_url=(BASE_URL + 'media/%s/comments') %(media_id)
-    payload={"access_token:":APP_ACCESS_TOKEN,"text" : comment_txt}
     print 'POST request url : %s ' %(request_url)
     comment_on_post=requests.post(request_url,payload).json()
     print comment_on_post
-
-    if comment_on_post['meta']['code']==200:
+    if comment_on_post['meta']['code'] == 200:
         print'successfully add a new comment'
     else:
         print"unable to comment .. please Try again..!"
