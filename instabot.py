245caefa509df5d84383f13835d570feadbaa845
shivam-walia <shivamw65@gmail.com>
2017-07-10 18:52:01 +0530

    Instabot calls the Test_choice and starts the bot application

--- instabot.py
+++ instabot.py
@@ -1,42 +1,6 @@
-#main.py file is created
+                                                #Imports the test choice and display the menu options
+#import required files
+from Test_choice import start_my_bot
 
-#from self_info import  self_info
-#function calling
-#self_info()
-#from  get_user_info import get_user_info
-#insta_username=raw_input("Enter the user name of user:")
-#get_user_info(insta_username)
-
-#from get_user_id import get_user_id
-#insta_username = raw_input("Enter the username of the user: ")
-#varq=get_user_id(insta_username)
-#print varq
-#function call for own post
-#from get_my_post import get_my_post
-#get_my_post()
-
-
-#function calling for user post
-
-#from get_user_post import get_user_post
-#insta_username=raw_input("Please enter user name")
-#get_user_post(insta_username)
-
-#from like_post import like_a_post
-#username="Rahul"
-#like_a_post(username)
-#function calling for post comments
-#from post_comments import  post_comment
-#insta_username=raw_input("Enter the username")
-#post_comment(insta_username)
-
-#function call for delete negative comments
-#from Negtive_comments_delete import delete_negative
-#insta_username=raw_input("enter user name")
-#delete_negative(insta_username)
-
-#from delete_comments import delete_comment
-#insta_username=raw_input("Enter  user name")
-#delete_comment(insta_username)
-
-
+#function calling ti start bot application
+start_my_bot()
