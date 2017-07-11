5969e2aa3d3c70c3af43675f05a9d690dc861da4
shivam-walia <shivamw65@gmail.com>
2017-07-11 12:19:46 +0530

    Objective completed displays user intrests on pie chart

--- objective2.py
+++ objective2.py
@@ -1,13 +1,34 @@
+#                                                      objective Plot user instrests using matplotlib  pie chart
+
+
+
+#import the required files
 import matplotlib.pyplot as plt
+import re
+#function defination
+def pichart():
 
-# Pie chart, where the slices will be ordered and plotted counter-clockwise:
-labels = 'Frogs', 'Hogs', 'Dogs', 'Logs'
-sizes = [15, 30, 45, 10]
-explode = (0, 0.1, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
+        #opens the file caption.txt
 
-fig1, ax1 = plt.subplots()
-ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
-        shadow=True, startangle=90)
-ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+        file=open("caption.txt",'r')
+        text=file.read()
+        read=re.sub(r'#'," ",text)
+        #results in result variable
+        result=read.split()
 
-plt.show()
+        size=str(result.count("instapic"))+'0'
+        size1=str(result.count("widfrnds"))+'0'
+        size2=str(result.count("widfrnd"))+'0'
+
+
+        #Pie chart, where the slices will be ordered and plotted counter-clockwise:
+        labels="Instagra_pic","weekend_enjoy","movie with friends"
+        sizes = [size,size1,size2]
+        explode = (0,0,0)  # only "explode" the 2nd slice (i.e. 'Hogs')
+
+        fig1, ax1 = plt.subplots()
+        ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+                shadow=True, startangle=90)
+        ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+        plt.show()
+
