04823de76c39f93c045905f62f1a4dee44bca606
shivam-walia <shivamw65@gmail.com>
2017-07-13 12:11:58 +0530

    sandbox users

---
+++ sandbox_users.py
@@ -0,0 +1,11 @@
+#The list of the sandbox users
+#Function defination
+def sandbox_user():
+    #Displays the sandbox users
+    print "\n These are the list of sandbox users \n"
+    print "\n 1. The admin-shivam.walia \n"
+    print"\n 2.rahul_r2557 \n"
+    print "\n 3.svrankit \n"
+#function call
+
+sandbox_user()
